16eb57591ca2fee94195949af93c94b870a2c10c
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-22 08:39:41 +0100

    Add files via upload

---
+++ protekloiTekućeVrijeme/protekloiTrenutnoVrijeme/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace protekloiTrenutnoVrijeme
+{
+    public partial class Form1 : Form
+    {
+        DateTime pocetak = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetak;
+            toolStripStatusLabel1.Text = "Proteklo: " +proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
